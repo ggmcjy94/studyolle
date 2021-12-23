7fbc951cf72a22f36a3e1813e2552efe3d7ba38f
ggmcjy <ggmcjy94@naver.com>
2021-12-24 00:25:46 +0900

    Initial commit

---
+++ src/main/java/com/studyolle/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.studyolle.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity // 내가 security 를 직접 다설정 하겠다.
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/","/login","/sign-up","/check-email","/check-email-token",
+                        "email-login","/check-email-login","/login-link").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .anyRequest().authenticated();
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+}
---
+++ src/main/java/com/studyolle/domain/Account.java
@@ -0,0 +1,53 @@
+package com.studyolle.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@EqualsAndHashCode(of = "id")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true) //같은 이메일, 닉네임 중복 방지 유일 해야함
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location; //varchar(255)
+
+    @Lob @Basic(fetch = FetchType.EAGER) // @Lob (크기)
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail;
+    private boolean studyUpdatedByWeb;
+}
